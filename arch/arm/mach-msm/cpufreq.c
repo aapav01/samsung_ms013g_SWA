c7e305ca8519e44c415d88c98e3aca3cf5f3486d
Paul Reioux <reioux@gmail.com>
2014-07-23 12:19:59 -0500

    arch/arm/mach-msm/cpufreq.c: remove legacy intellidemand leftovers
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -484,7 +484,6 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	INIT_WORK(&cpu_work->work, set_cpu_work);
 	init_completion(&cpu_work->complete);
 #endif
-
 	return 0;
 }
 
