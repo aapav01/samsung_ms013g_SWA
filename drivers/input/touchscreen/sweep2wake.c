12f088e9e1efa5bbced5a1efd71893f6aca61816
savoca <adeddo27@gmail.com>
2014-07-26 15:08:01 +0000

    s2w: Use s2s-only (s2w_switch == 2) properly
    Conflicts:
    	drivers/input/touchscreen/synaptics_dsx_i2c.c

--- drivers/input/touchscreen/sweep2wake.c
+++ drivers/input/touchscreen/sweep2wake.c
@@ -498,7 +498,7 @@ static ssize_t s2w_sweep2wake_show(struct device *dev,
 static ssize_t s2w_sweep2wake_dump(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
-	if (buf[0] >= '0' && buf[0] <= '1' && buf[1] == '\n')
+	if (buf[0] >= '0' && buf[0] <= '2' && buf[1] == '\n')
                 if (s2w_switch != buf[0] - '0')
 		        s2w_switch = buf[0] - '0';
 
