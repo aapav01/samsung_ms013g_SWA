a92aaa8652f568bebee16d027256755c48253d08
Apavayan <info@apavayan.com>
2015-06-25 17:34:57 +0530

    v2 build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,9 +7,9 @@ mkdir output
 make -C $(pwd) O=output VARIANT_DEFCONFIG=ms013g_defconfig ms013g_tubro_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -C $(pwd) O=output
 
-#tools/dtbTool -s 2048 -o output/arch/arm/boot/dt.img -p output/scripts/dtc/ output/arch/arm/boot/
+tools/dtbTool -s 2048 -o output/arch/arm/boot/dt.img -p output/scripts/dtc/ output/arch/arm/boot/
 
 cp -v output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
 
-#mkdir out_modules
-#find ./ -name "*.ko" -exec cp {} out_modules \
+mkdir out_modules
+find ./ -name "*.ko" -exec cp {} out_modules \;
