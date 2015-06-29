0b6dc9803886b73b14b96259fb66c26ba8a1cd8e
Apavayan <info@apavayan.com>
2015-06-29 19:29:40 +0530

    unknow commit - TEST

--- arch/arm/mach-msm/acpuclock-8226.c
+++ arch/arm/mach-msm/acpuclock-8226.c
@@ -38,9 +38,11 @@ static struct msm_bus_paths bw_level_tbl_8226[] = {
 	[2] =  BW_MBPS(400), /* At least 50 MHz on bus. */
 	[3] =  BW_MBPS(800), /* At least 100 MHz on bus. */
 	[4] = BW_MBPS(1600), /* At least 200 MHz on bus. */
-	[5] = BW_MBPS(2128), /* At least 266 MHz on bus. */
+	[5] = BW_MBPS(2664), /* At least 333 MHz on bus. */
 	[6] = BW_MBPS(3200), /* At least 400 MHz on bus. */
 	[7] = BW_MBPS(4264), /* At least 533 MHz on bus. */
+	[8] = BW_MBPS(4528), /* At least 566 MHz on bus. */
+	[9] = BW_MBPS(4800), /* At least 600 MHz on bus. */
 };
 
 static struct msm_bus_paths bw_level_tbl_8610[] = {
@@ -61,83 +63,102 @@ static struct msm_bus_scale_pdata bus_client_pdata = {
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
 #ifdef CPU_UNDERCLOCK
-	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 #endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
-	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
-	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4, 0, 6 },
-	{ 1,  998400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
-	{ 1, 1094400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
-	{ 0, 1190400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
+	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4,    0, 6 },
+	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4,    0, 6 },
+	{ 1,  998400, ACPUPLL, 5, 0,   CPR_CORNER_12,   0, 7 },
+	{ 1, 1094400, ACPUPLL, 5, 0,   CPR_CORNER_12,   0, 7 },
+	{ 1, 1190400, ACPUPLL, 5, 0,   CPR_CORNER_7,    0, 7 },
+	{ 1, 1305600, ACPUPLL, 5, 0,   CPR_CORNER_8,    0, 8 },
+	{ 1, 1344000, ACPUPLL, 5, 0,   CPR_CORNER_9,    0, 8 },
+	{ 1, 1401600, ACPUPLL, 5, 0,   CPR_CORNER_10,   0, 9 },
+	{ 1, 1497600, ACPUPLL, 5, 0,   CPR_CORNER_11,   0, 9 },
+	{ 1, 1593600, ACPUPLL, 5, 0,   CPR_CORNER_12,   0, 9 },
 	{ 0 }
 };
 
+#ifdef CONFIG_USERSPACE_VOLTAGE_CONTROL
+static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
+	{ 1,  300000, PLL0,    4, 2,   1140000,    1140000, 4 },
+	{ 1,  384000, ACPUPLL, 5, 2,   1140000,    1140000, 4 },
+	{ 1,  600000, PLL0,    4, 0,   1140000,    1140000, 6 },
+	{ 1,  787200, ACPUPLL, 5, 0,   1140000,    1140000, 6 },
+	{ 1,  998400, ACPUPLL, 5, 0,   1150000,    1150000, 7 },
+	{ 1, 1094400, ACPUPLL, 5, 0,   1150000,    1150000, 7 },
+	{ 1, 1190400, ACPUPLL, 5, 0,   1150000,    1150000, 7 },
+	{ 1, 1305600, ACPUPLL, 5, 0,   1280000,    1280000, 8 },
+	{ 1, 1344000, ACPUPLL, 5, 0,   1280000,    1280000, 8 },
+	{ 1, 1401600, ACPUPLL, 5, 0,   1280000,    1280000, 9 },
+	{ 1, 1497600, ACPUPLL, 5, 0,   1280000,    1280000, 9 },
+	{ 1, 1593600, ACPUPLL, 5, 0,   1280000,    1280000, 9 },
+	{ 0 }
+};
+#else
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
 #ifdef CPU_UNDERCLOCK
-	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 #endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
-	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
-	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4, 0, 6 },
-	{ 1,  998400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
-	{ 1, 1094400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
-	{ 1, 1190400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
+	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 	0, 6 },
+	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4, 	0, 6 },
+	{ 1,  998400, ACPUPLL, 5, 0,   CPR_CORNER_12,  	0, 7 },
+	{ 1, 1094400, ACPUPLL, 5, 0,   CPR_CORNER_12,  	0, 7 },
+	{ 1, 1190400, ACPUPLL, 5, 0,   CPR_CORNER_12,  	0, 7 },
 	{ 0 }
 };
 
+#endif
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
 #ifdef CPU_UNDERCLOCK
-	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 #endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
-	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
-	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4, 0, 6 },
-	{ 1,  998400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
-	{ 1, 1094400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
-	{ 1, 1190400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
-	{ 1, 1305600, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
-	{ 1, 1344000, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
-	{ 1, 1401600, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
+	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 	0, 6 },
+	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4, 	0, 6 },
+	{ 1,  998400, ACPUPLL, 5, 0,   CPR_CORNER_12,  	0, 7 },
+	{ 1, 1094400, ACPUPLL, 5, 0,   CPR_CORNER_12,  	0, 7 },
+	{ 1, 1190400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 8 },
+	{ 1, 1305600, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 8 },
+	{ 1, 1344000, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 9 },
+	{ 1, 1401600, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 9 },
 	{ 0 }
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p6[] = {
 #ifdef CPU_UNDERCLOCK
-	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 #endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
-	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
-	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4, 0, 6 },
-	{ 1,  998400, ACPUPLL, 5, 0,   CPR_CORNER_5,  0, 7 },
-	{ 1, 1094400, ACPUPLL, 5, 0,   CPR_CORNER_6,  0, 7 },
-	{ 1, 1190400, ACPUPLL, 5, 0,   CPR_CORNER_7,  0, 7 },
-	{ 1, 1305600, ACPUPLL, 5, 0,   CPR_CORNER_8,  0, 7 },
-	{ 1, 1344000, ACPUPLL, 5, 0,   CPR_CORNER_9,  0, 7 },
-	{ 1, 1401600, ACPUPLL, 5, 0,   CPR_CORNER_10,  0, 7 },
-	{ 1, 1497600, ACPUPLL, 5, 0,   CPR_CORNER_11,  0, 7 },
-	{ 1, 1593600, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 7 },
+	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 	0, 6 },
+	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4, 	0, 6 },
+	{ 1,  998400, ACPUPLL, 5, 0,   CPR_CORNER_5,  	0, 7 },
+	{ 1, 1094400, ACPUPLL, 5, 0,   CPR_CORNER_6,  	0, 7 },
+	{ 1, 1190400, ACPUPLL, 5, 0,   CPR_CORNER_7,   	0, 8 },
+	{ 1, 1305600, ACPUPLL, 5, 0,   CPR_CORNER_8,   	0, 8 },
+	{ 1, 1344000, ACPUPLL, 5, 0,   CPR_CORNER_9,   	0, 9 },
+	{ 1, 1401600, ACPUPLL, 5, 0,   CPR_CORNER_10,  	0, 9 },
+	{ 1, 1497600, ACPUPLL, 5, 0,   CPR_CORNER_11,  	0, 9 },
+	{ 1, 1593600, ACPUPLL, 5, 0,   CPR_CORNER_12,  	0, 9 },
 	{ 0 }
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8610[] = {
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 3 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
-	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 4 },
-	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4, 0, 4 },
-	{ 1,  998400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 5 },
-	{ 1, 1190400, ACPUPLL, 5, 0,   CPR_CORNER_12,  0, 5 },
+	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 	0, 4 },
+	{ 1,  787200, ACPUPLL, 5, 0,   CPR_CORNER_4, 	0, 4 },
+	{ 1,  998400, ACPUPLL, 5, 0,   CPR_CORNER_12,  	0, 5 },
+	{ 1, 1190400, ACPUPLL, 5, 0,   CPR_CORNER_12,  	0, 8 },
 	{ 0 }
 };
 
@@ -155,7 +176,11 @@ static struct acpuclk_drv_data drv_data = {
 	.freq_tbl = acpu_freq_tbl_8226_1p1,
 	.pvs_tables = pvs_tables_8226,
 	.bus_scale = &bus_client_pdata,
-	.vdd_max_cpu = CPR_CORNER_12,
+#ifdef CONFIG_USERSPACE_VOLTAGE_CONTROL
+	.vdd_max_cpu = 1280000,
+#else
+ 	.vdd_max_cpu = CPR_CORNER_12,
+#endif
 	.src_clocks = {
 		[PLL0].name = "gpll0",
 		[ACPUPLL].name = "a7sspll",
@@ -168,8 +193,13 @@ static struct acpuclk_drv_data drv_data = {
 		.update_mask = RCG_CONFIG_UPDATE_BIT,
 		.poll_mask = RCG_CONFIG_UPDATE_BIT,
 	},
+#ifdef CONFIG_CPU_UNDERCLOCK
+	.power_collapse_khz = 192000,
+	.wait_for_irq_khz = 192000,
+#else
 	.power_collapse_khz = 300000,
 	.wait_for_irq_khz = 300000,
+#endif
 };
 
 static int __init acpuclk_a7_probe(struct platform_device *pdev)
