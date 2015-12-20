1d21cdacae13e584b91177c745b7512a517ffd0f
Apavayan Sinha <info@apavayan.com>
2015-12-20 17:55:33 +0530

    msm: fix typo

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -335,7 +335,7 @@ void __init msm8226_add_drivers(void)
 	platform_device_register(&sec_device_thermistor);
 #endif
 #endif
-#ifndef CONFIG_INTELLI_THERMAL || CONFIG_INTELLI_THERMAL_V2
+#if !defined(CONFIG_INTELLI_THERMAL) || !defined(CONFIG_INTELLI_THERMAL_V2)
 	msm_thermal_device_init();
 #endif
 #ifdef CONFIG_LCD_KCAL
